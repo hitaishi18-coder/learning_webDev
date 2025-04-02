2406fd31cc5b67c54ad9c0c9abbca4026cde391c
hitaishi18-coder <hitaishi.8.8.8@gmail.com>
2025-04-02 15:32:38 +0530

    first commit

---
+++ javascript/script.js
@@ -0,0 +1,55 @@
+// console.log("hello world");
+
+// alert("hello world");
+
+
+
+// console.log(document.getElementById("heading"));
+
+
+// document.getElementById("heading").innerText="hello world";
+// document.getElementById("heading").style.color="blue";
+// document.getElementById("heading").style.backgroundColor = "pink";
+
+
+
+// 1
+
+var h1Element = document.getElementById("heading");
+
+
+
+console.log(h1Element)
+
+
+h1Element.innerText = " hello world";
+h1Element.style.color = " blue";
+h1Element.style.backgroundColor="pink";
+
+
+
+
+//   2 
+
+var fullName = " rahul";
+var age = 22;
+var isAdult = true ;
+var user = {
+    name : "hitaishi ",
+    age : 22 
+}
+
+
+console.log(fullName); // white color 
+console.log(age);      // blue color 
+console.log(isAdult);
+console.log(user);
+
+console.log(typeof user);
+console.log(typeof fullName);
+
+
+document.getElementById("head").innerHTML = "hello <span> john </span"
+
+
+
---
+++ javascript/script2.js
@@ -0,0 +1,24 @@
+
+// function changePhoto(){
+
+//     document.getElementById("photo").src = "xyz.webp";
+//     document.getElementById("photo").style.border = " 3px solid black ";
+//     document.getElementById("photo").style.width = "500px";
+//     document.getElementById("photo").style.height= "500px";
+// }
+
+
+kkk
+
+// or 
+
+
+
+function changePhoto() {
+    let imageTag = document.getElementById("photo")
+    imageTag.src = "xyz.webp";
+    imageTag.style.border = " 3px solid black ";
+    imageTag.style.width = " 300px "; 
+
+}
+
